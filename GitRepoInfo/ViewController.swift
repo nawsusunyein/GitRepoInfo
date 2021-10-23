1c49654aeba376e70985e3e522de2102492c3ca7
Naw Su Su Nyein <nawsusunyein@gmail.com>
2021-10-23 13:31:50 +0630

    create project

---
+++ GitRepoInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitRepoInfo
+//
+//  Created by Naw Su Su Nyein on 10/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
